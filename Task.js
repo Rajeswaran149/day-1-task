1c35287e3c2e8c3f5667f9c76e2b4636dc63463f
Rajeswaran149 <rajeswaran149@gmail.com>
2022-01-24 22:12:11 -0800

    guvi_task

---
+++ Task.js
@@ -0,0 +1,15 @@
+class Employee {
+    constructor(companyName,employeeName) {
+      this.companyName = Name;
+      this.employeeName = Role;
+    }
+    addEmployee(){
+    console.log(
+        `new emlpoyee ${this.employeeName} added to the company ${this.companyName}`
+    );
+    }
+}
+
+const a =new Employee("Raj","rrr");
+a.addEmployee();
+console.log("hello");
